87b356330e9bcc007c753e26f58499941c24a4a4
samurayjedi <samurayjedi@piwi.piwis.net>
2024-11-17 16:26:09 -0400

    Add imperative handle to drawer, to get overlay and access to its anim state variables.

--- src/Drawer/index.tsx
+++ src/Drawer/index.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useCallback, useRef, useEffect } from 'react';
+import React, {
+  useState,
+  useCallback,
+  useRef,
+  useEffect,
+  useImperativeHandle,
+} from 'react';
 import styled from '@emotion/native';
 import {
   ViewProps,
@@ -8,50 +14,56 @@ import {
 import Constants from 'expo-constants';
 import DrawerOverlay, { OverlayInterface, OverlayProps } from './Overlay';
 
-function Drawer({ open, items, effect = 3, colors, ...props }: DrawerProps) {
-  const overlayRef = useRef<OverlayInterface>(null);
-  const [display, setDisplay] = useState(false);
+export default React.forwardRef<Ref, DrawerProps>(
+  ({ open, items, effect = 3, colors, ...props }, ref) => {
+    const overlayRef = useRef<OverlayInterface>(null);
+    const [display, setDisplay] = useState(false);
 
-  useEffect(() => {
-    if (overlayRef.current && !overlayRef.current.overlays.isAnimating) {
-      if (open) {
-        if (!overlayRef.current.overlays.isOpened) {
-          overlayRef.current.overlays.toggle();
-        }
-      } else {
-        if (overlayRef.current.overlays.isOpened) {
-          overlayRef.current.overlays.toggle();
+    useEffect(() => {
+      if (overlayRef.current && !overlayRef.current.overlays.isAnimating) {
+        if (open) {
+          if (!overlayRef.current.overlays.isOpened) {
+            overlayRef.current.overlays.toggle();
+          }
+        } else {
+          if (overlayRef.current.overlays.isOpened) {
+            overlayRef.current.overlays.toggle();
+          }
         }
       }
-    }
-  }, [open]);
+    }, [open]);
 
-  const onOpenOverlay = useCallback(() => setDisplay(true), []);
-  const onCloseOverlay = useCallback(() => setDisplay(false), []);
+    useImperativeHandle(ref, () => ({
+      ref: overlayRef,
+    }));
 
-  return (
-    <Root display={display} {...props}>
-      <Wrapper>
-        <DrawerOverlay
-          ref={overlayRef}
-          effect={effect}
-          colors={colors}
-          onOpen={onOpenOverlay}
-          onClose={onCloseOverlay}
-        />
-        {open && (
-          <Content>
-            <Glue />
-            {items.map(([key, label]) => (
-              <Item key={key}>{label}</Item>
-            ))}
-            <Glue />
-          </Content>
-        )}
-      </Wrapper>
-    </Root>
-  );
-}
+    const onOpenOverlay = useCallback(() => setDisplay(true), []);
+    const onCloseOverlay = useCallback(() => setDisplay(false), []);
+
+    return (
+      <Root display={display} {...props}>
+        <Wrapper>
+          <DrawerOverlay
+            ref={overlayRef}
+            effect={effect}
+            colors={colors}
+            onOpen={onOpenOverlay}
+            onClose={onCloseOverlay}
+          />
+          {open && (
+            <Content>
+              <Glue />
+              {items.map(([key, label]) => (
+                <Item key={key}>{label}</Item>
+              ))}
+              <Glue />
+            </Content>
+          )}
+        </Wrapper>
+      </Root>
+    );
+  },
+);
 
 export interface DrawerProps extends ViewProps {
   open: boolean;
@@ -60,8 +72,6 @@ export interface DrawerProps extends ViewProps {
   effect?: OverlayProps['effect'];
   colors: OverlayProps['colors'];
 }
-
-export default Drawer;
 
 function Item({ children, ...props }: TouchableWithoutFeedbackProps) {
   return (
@@ -106,3 +116,7 @@ const MenuItem = styled.Text(({ theme }) => ({
 const Glue = styled.View({
   flex: 1,
 });
+
+interface Ref {
+  ref: React.RefObject<OverlayInterface>;
+}
