88bfb10dd013efe040284143892a49fa644c226b
samurayjedi <samurayjedi@piwi.piwis.net>
2025-05-13 15:12:39 -0400

    Fixing borders when variant='text'

--- src/ButtonGroup.tsx
+++ src/ButtonGroup.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import styled from '@emotion/native';
 import type { ButtonProps } from './Button';
+import { useTheme } from '@emotion/react';
+import { getColor } from './styles';
 
 export default function ButtonGroup({
   children,
   style,
   orientation = 'horizontal',
+  /** from Button */
+  variant = 'text',
+  color = 'primary',
+  size = 'medium',
   ...props
 }: ButtonGroupProps) {
+  const theme = useTheme();
+
   return (
     <Container orientation={orientation}>
       {React.Children.map(children, (child, i) => {
         if (React.isValidElement(child)) {
           const newProps = {
             ...props,
+            variant,
+            color,
+            size,
             style: (() => {
               if (i === 0) {
                 if (orientation === 'horizontal') {
@@ -21,12 +32,24 @@ export default function ButtonGroup({
                     borderRadius: 0,
                     borderTopLeftRadius: 4,
                     borderBottomLeftRadius: 4,
+                    ...(variant === 'text'
+                      ? {
+                          borderRightWidth: 1,
+                          borderColor: getColor(theme, color),
+                        }
+                      : {}),
                   };
                 } else {
                   return {
                     borderRadius: 0,
                     borderTopLeftRadius: 4,
                     borderTopRightRadius: 4,
+                    ...(variant === 'text'
+                      ? {
+                          borderBottomWidth: 1,
+                          borderColor: getColor(theme, color),
+                        }
+                      : {}),
                   };
                 }
               } else if (i === React.Children.count(children) - 1) {
@@ -52,6 +75,17 @@ export default function ButtonGroup({
                 ...(orientation === 'horizontal'
                   ? { borderLeftWidth: 0 }
                   : { borderTopWidth: 0 }),
+                ...(variant === 'text'
+                  ? orientation === 'horizontal'
+                    ? {
+                        borderRightWidth: 1,
+                        borderColor: getColor(theme, color),
+                      }
+                    : {
+                        borderBottomWidth: 1,
+                        borderColor: getColor(theme, color),
+                      }
+                  : {}),
               };
             })(),
           };
